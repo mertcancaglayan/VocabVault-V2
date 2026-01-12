0e50b3b9505f59d9e99c9c900968931bc0233803
M.can <caglayan.mertcan37@gmail.com>
2026-01-12 17:33:10 +0300

    add loading to homepage

--- frontend/src/components/categories/CategoryContainer.tsx
+++ frontend/src/components/categories/CategoryContainer.tsx
@@ -3,13 +3,29 @@ import "../categories/CategoryContainer.css"
 import { getCategoriesV2 } from "../../api/api"
 import Slider from "./slider/Slider"
 import type { CategoryDocument } from "../../models/models"
+import Spinner from "../ui/spinner/Spinner"
 
 function CategoryContainer() {
     const [categories, setCategories] = useState<CategoryDocument[]>([])
+    const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
-        getCategoriesV2().then(data => setCategories(data)).catch(err => console.error(err))
+        setIsLoading(true)
+
+        getCategoriesV2()
+            .then(data => {
+                setCategories(data)
+            })
+            .catch(err => {
+                console.error(err)
+            })
+            .finally(() => {
+                setIsLoading(false)
+            })
     }, [])
+
+
+    if (isLoading) return <Spinner message={"Loading categories..."}></Spinner>;
 
     return (
         <section className="categories-sliders">
