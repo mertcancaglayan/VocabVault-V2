c7b634a9e5e573a3900c9802f20bfe4f1524a3f9
M.can <caglayan.mertcan37@gmail.com>
2026-01-22 16:15:35 +0300

    create greeting custom hook

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,23 +3,18 @@ import "../../src/index.css";
 import CategoryContainer from "../components/categories/CategoryContainer";
 import GameModesComponent from "../components/gameModes/GameModes";
 import Header from "../components/header/Header";
-import { useCallback, useContext, useState } from "react";
+import { useContext } from "react";
 import AppContext from "../context/AppContext";
 import GreetingsSection from "../components/greetings/Greetings";
 import { buttonTexts } from "../data/buttonTexts";
 import { FaPlay } from "react-icons/fa";
+import { useGreetingState } from "../hooks/useGreetingState";
 
 function Home() {
-    const [isGreeted, setIsGreeted] = useState(() => sessionStorage.getItem("greeted") === "true");
     const navigate = useNavigate();
+    const { isGreeted, handleContinue } = useGreetingState()
 
     const contextValue = useContext(AppContext);
-
-    const handleContinue = useCallback(() => {
-        sessionStorage.setItem("greeted", "true")
-        setIsGreeted(true)
-    }, []);
-
     if (!contextValue) {
         throw new Error("Home must be used within AppProvider");
     }
